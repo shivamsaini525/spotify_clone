3873ef6a6f26e2df562f21532d552777757dd759
shivam <sainishivamsaini12345@gmail.com>
2024-03-05 23:03:10 +0530

    artistplaylisjjjt iks completle

--- movix/src/App.jsx
+++ movix/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <>
      <h1>hello</h1>
-     <p>I'm Nikita Sejwal, thrilled to connect! At Acqux, we noticed your interest in our open position. Elevate your career by clicking the link below and filling out our application form using my exclusive employee code "AC035MKT" for priority processing. Your journey with us awaits! Looking forward to your incredible potential.
+     <p>I'm Nikita Sejwal, thrilled to jj connect! At Acqux, we noticed your interest in our open position. Elevate your career by clicking the link below and filling out our application form using my exclusive employee code "AC035MKT" for priority processing. Your journey with us awaits! Looking forward to your incredible potential.
 
 Best,
 Nikita Sejwal
