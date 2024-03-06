639e34a5a271a6e75973b6b76054a38adde12969
shivam <sainishivamsaini12345@gmail.com>
2024-03-06 11:51:13 +0530

    artistplaylisjjt is completed

--- movix/src/App.jsx
+++ movix/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';ss
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
@@ -7,11 +7,14 @@ const App = () => {
   return (
     <>
      <h1>hello</h1>
-     <p>I'm Nikita Sejwal, thrilled to jj connect! At Acqux,j we noticed your interest in our open position. Elevate your career by clicking the link below and filling out our application form using my exclusive employee code "AC035MKT" for priority processing. Your journey with us awaits! Looking forward to your incredible potential.
-
-Best,
-Nikita Sejwal
-<b>https://acqux.com/careers/</b></p>
+      <p>I'm Nikita Sejwal, thrilled to jj connect! At Acqux,j we
+        noticed your interest in our open position. Elevate your 
+        career by clicking ff the link below and filling out our application 
+        form using my exclusive employee code <b>"AC035MKT"</b> for priority processing. Your 
+        journey with us awaits! Looking forward to your incredible potential Best,
+        Nikita Sejwal
+        <b> <a href="https://acqux.com/careers/">Send Apllication </a></b>
+      </p>
     </>
   );
 };
