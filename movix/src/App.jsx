87cb82684fb88514087b31420ce1703fd767bbf1
shivam <sainishivamsaini12345@gmail.com>
2024-03-07 14:07:26 +0530

    Router is steup

--- movix/src/App.jsx
+++ movix/src/App.jsx
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
 const App = () => {
 
 
   return (
     <>
-     <h1>hello</h1>
-      <p>I'm Nikita Sejwal, thrilled to jj connect! At Acqux,j we
-        noticed your interest in dsd in our open position. Elevate your 
-        career by clicking ff the link below and filling out our s s  application 
-        form using my exclusive employee code <b>"AC035MKT"</b> for priority processing. Your 
-        journey with us awaits! Looking forward to your incredible potential Best,dd
-        Nikita Sejwal
-        <b> <a href="https://acqux.com/careers/">Send Apllication </a></b>
-      </p>
+    <div className="App">
+     <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>hello</div>} />
+          <Route path="/playlist" element={<Navbar />} />
+        </Routes>
+     </BrowserRouter>
+     </div>
     </>
   );
 };
---
+++ movix/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar=()=>{
+
+    return(
+        <>
+            <div>
+                <h1>Heellooo babess</h1>
+            </div>
+        </>
+    )
+}
+export default Navbar
