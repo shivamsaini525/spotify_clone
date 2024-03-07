cc7ed6f084211424408d3c88b924ed69e51f53a1
shivam <sainishivamsaini12345@gmail.com>
2024-03-07 17:23:18 +0530

    login form in processing

--- movix/src/components/LoginComponent.jsx
+++ movix/src/components/LoginComponent.jsx
@@ -1,9 +1,20 @@
 import { Icon } from '@iconify-icon/react';
+// import style from "./LoginComponent.module.css";
+
 const LoginComponent =()=>{
   return(
     <>
-       <div className="w-full h-full">
-       <Icon icon="logos:spotify" width="165" />
+       <div className="w-full h-full flex flex-col items-center">
+       <div className='logo p-5 border-b border-solid border-gray-300 w-full flex justify-center'>
+         <Icon icon="logos:spotify" width="145" /> 
+        </div>
+        <div className='inputRegion'>
+             <form>
+                <label type="text" className='block text-sm font-medium leading-6 text-gray-900'>Email</label>
+                <input type="email" className='p-2 border border-solid border-black-400' placeholder='Enter your email addresas' name="email"></input>
+             </form>
+        </div>
+       
        </div>
     </>
   )
