6bea23e165313ea1626e75ff7ce4f2d2c68db2d2
shivam <sainishivamsaini12345@gmail.com>
2024-03-07 15:00:45 +0530

    iconfy  is steup

--- movix/src/App.jsx
+++ movix/src/App.jsx
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
+
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Navbar from './components/Navbar';
+import LoginComponent from './components/LoginComponent';
+import "./output.css";
 const App = () => {
 
-
+ 
   return (
     <>
-    <div className="App">
-     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<div>hello</div>} />
-          <Route path="/playlist" element={<Navbar />} />
-        </Routes>
-     </BrowserRouter>
+    <div className="w-screen h-screen">
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<div className="bg-blue-500">hello</div>} />
+            <Route path="/login" element={<LoginComponent />} />
+          </Routes>
+      </BrowserRouter>
      </div>
     </>
   );
---
+++ movix/src/components/LoginComponent.jsx
@@ -0,0 +1,11 @@
+import { Icon } from '@iconify-icon/react';
+const LoginComponent =()=>{
+  return(
+    <>
+       <div className="w-full h-full">
+       <Icon icon="logos:spotify" width="165" />
+       </div>
+    </>
+  )
+}   
+export default LoginComponent
