f307b33f159c6148bb13cebac6b2d9090de3dd48
shivam <sainishivamsaini12345@gmail.com>
2024-03-11 21:38:08 +0530

    home page sidebar

--- movix/src/routes/HomeComponent.jsx
+++ movix/src/routes/HomeComponent.jsx
@@ -1,4 +1,7 @@
 import spotify_logo from "../assets/spotify_logo_white.svg";
+import IconText from "../components/shared/IconText";
+import { Icon } from '@iconify-icon/react';
+import { TiHome } from "react-icons/ti";
 const HomeComponent=()=>{
     return(
         <>
@@ -6,6 +9,13 @@ const HomeComponent=()=>{
                 <div className="h-full w-1/5 bg-black">
                    <div className="logoDiv p-6">
                        <img src={spotify_logo} alt="spotify logo" width={125} />
+                   </div>
+                   <div>
+                    
+                    <IconText iconName={"material-symbols-light:home"} displayText={"Home"} active/>
+                    <IconText iconName={"material-symbols:search"}   displayText={"Search"}/>
+                    
+                    <IconText iconName={"icomoon-free:books"} displayText={"Library"}/>
                    </div>
                 </div>
                 <div className="h-full">
