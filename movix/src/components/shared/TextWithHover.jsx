154565366bc39eefc90ce3a7c92a9c8ab2f8434c
shivam <sainishivamsaini12345@gmail.com>
2024-03-12 16:15:58 +0530

    navbar is complete bro

---
+++ movix/src/components/shared/TextWithHover.jsx
@@ -0,0 +1,17 @@
+
+
+const TextWithHover=({displayText, active})=>{
+    return(
+        <>
+        <div className="flex items-center justify-start cursor-pointer">
+           
+            <div className={`${active?"text-white":"text-gray-400"} text-lg font-semibold hover:text-white`}>
+                {displayText}
+            </div>
+        </div>
+
+        </> 
+
+    )
+}
+export default TextWithHover
--- movix/src/routes/HomeComponent.jsx
+++ movix/src/routes/HomeComponent.jsx
@@ -2,6 +2,7 @@ import spotify_logo from "../assets/spotify_logo_white.svg";
 import IconText from "../components/shared/IconText";
 import { Icon } from '@iconify-icon/react';
 import { TiHome } from "react-icons/ti";
+import TextWithHover from "../components/shared/TextWithHover";
 const HomeComponent=()=>{
     return(
         <>
@@ -29,8 +30,30 @@ const HomeComponent=()=>{
                         </div>
                     </div>
                 </div>
-                <div className="h-full">
-                   main content
+
+                
+                <div className="h-full w-4/5 bg-app-black ">
+                    <div className="navbar flex items-center justify-end w-full bg-black text-white h-1/10 bg-opacity-30" style={{"justifyContent":"end"}}>
+                        <div className="w-1/2 flex h-full ">
+                            <div className="w-3/5 flex justify-around items-center ">
+                                <TextWithHover displayText={"Premium"}/>
+                                <TextWithHover displayText={"Support"}/>
+                                <TextWithHover displayText={"Download"}/>
+                                <div className="h-1/2 border-r border-white"></div>
+                            </div>
+                            <div className="w-2/5 flex justify-around h-full items-center">
+                                <TextWithHover displayText={"Sign up"}/>
+                                <div className="">
+                                    <button className="bg-white h-2/3 px-8 flex items-center justify-center rounded-full font-semibold text-black">Log In</button>
+                                </div>   
+                            </div>
+                            
+                            
+                        </div>
+                       
+                        
+                    </div> 
+                    <div className="content">main content</div>
                 </div>
             </div>
         </>
