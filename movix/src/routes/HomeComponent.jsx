d681ed90fb8d3b092702cd0cd76cd7c7ba8c20b3
shivam <sainishivamsaini12345@gmail.com>
2024-03-13 12:56:55 +0530

    card data is dynamic

--- movix/src/routes/HomeComponent.jsx
+++ movix/src/routes/HomeComponent.jsx
@@ -3,6 +3,35 @@ import IconText from "../components/shared/IconText";
 import { Icon } from '@iconify-icon/react';
 import { TiHome } from "react-icons/ti";
 import TextWithHover from "../components/shared/TextWithHover";
+
+const focusCardsData=[
+    {
+        title:"Peaceful Piano",
+        description: "Relax and indulge with beautiful piano pieces",
+        imgUrl: "https://images.unsplash.com/photo-1645207213625-aa6367cbab66?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+   },
+   {
+        title: "Deep Focus",
+        description: "Keep calm and focus with this music",
+        imgUrl: "https://images.unsplash.com/photo-1645860265927-02deb3fe4359?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+   },
+   {
+        title: "Instrumental Study",
+        description: "Focus with soft study music in the background ",
+        imgUrl: "https://images.unsplash.com/photo-1646734329499-0afb2db25fb3?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+   },
+   { 
+        title: "Focus Flow",
+        description: "Up tempo instrumental hip hop beats ",
+        imgUrl: "https://images.unsplash.com/photo-1645036993886-efdfd96ca97f?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+   },
+   {
+        title: "Beats to think to",
+        description: "Focus with deep base techno ",
+        imgUrl: "https://images.unsplash.com/photo-1645207213625-aa6367cbab66?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+   }
+];
+
 const HomeComponent=()=>{
     return(
         <>
@@ -53,10 +82,10 @@ const HomeComponent=()=>{
                        
                         
                     </div> 
-                    <div className="content text-white p-8 overflow-auto">
-                      <PlaylistView  titleText="Focus" />
-                      <PlaylistView  titleText="Spotify Playlist" />
-                      <PlaylistView  titleText="Sound of India" />
+                    <div className="content text-white p-8 pt-0 overflow-auto">
+                      <PlaylistView  titleText="Focus"  cardData={focusCardsData}/>
+                      <PlaylistView  titleText="Spotify Playlist" cardData={focusCardsData} />
+                      <PlaylistView  titleText="Sound of India" cardData={focusCardsData} />
                     </div>
                 </div>
             </div>
@@ -67,26 +96,35 @@ const HomeComponent=()=>{
 const PlaylistView=({titleText, cardData})=>{
     return(
         <>
-            <div className="text-white">
+            <div className="text-white mt-8">
                 <div className="text-2xl font-semibold mb-5">{titleText}</div>
                 <div className="w-full flex justify-between space-x-4">
-                    <Card title="Peaceful Piano" description="Relax and indulge with beautiful piano pieces" src="https://images.unsplash.com/photo-1633622800679-bf4dfaa260c1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-                    <Card title="Deep Focus" description="Keep calm and focus with this music" src="https://images.unsplash.com/photo-1645207213625-aa6367cbab66?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-                    <Card title="Instrumental Study" description="Focus with soft study music in the background" src="https://images.unsplash.com/photo-1645860265927-02deb3fe4359?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-                    <Card title="Focus Flow" description="Up tempo instrumental hip hop beats" src="https://images.unsplash.com/photo-1646734329499-0afb2db25fb3?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-                    <Card title="Beats to think to" description="Focus with deep base techno" src="https://images.unsplash.com/photo-1645036993886-efdfd96ca97f?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                   
+                   {
+                    cardData.map(item=>{
+                        return <Card title={item.title} description={item.description} imgUrl={item.imgUrl} /> 
+                    })
+                   };
+
+
+
+                    {/* <Card title="Peaceful Piano" description="Relax and indulge with beautiful piano pieces" imgUrl="https://images.unsplash.com/photo-1645207213625-aa6367cbab66?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                    <Card title="Deep Focus" description="Keep calm and focus with this music" imgUrl="https://images.unsplash.com/photo-1645207213625-aa6367cbab66?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                    <Card title="Instrumental Study" description="Focus with soft study music in the background" imgUrl="https://images.unsplash.com/photo-1645860265927-02deb3fe4359?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                    <Card title="Focus Flow" description="Up tempo instrumental hip hop beats" imgUrl="https://images.unsplash.com/photo-1646734329499-0afb2db25fb3?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                    <Card title="Beats to think to" description="Focus with deep base techno" imgUrl="https://images.unsplash.com/photo-1645036993886-efdfd96ca97f?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" /> */}
                 </div>
             </div>
         </>
     );
 };
 
-const Card=({title,description,src})=>{
+const Card=({title,description,imgUrl})=>{
     return( 
         <>
         <div className="bg-black bg-opacity-40 w-1/6 p-4 rounded-lg">
             <div className="pb-4 pt-2">
-                <img className="w-full rounded-md " src={src}  alt="label" />
+                <img className="w-full rounded-md " src={imgUrl}  alt="label" />
             </div>
             <div className="text-white  font-semibold py-3">{title}</div>
             <div className="text-gray-500 text-sm"> {description}</div>
